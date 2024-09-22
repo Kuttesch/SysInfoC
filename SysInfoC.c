b0c920cfeef139b346b7e11bf4094a2e3463911b
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-09-22 15:22:14 +0200

    Merged getCpuINfo into one function

--- SysInfoC.c
+++ SysInfoC.c
@@ -2,25 +2,6 @@
 #include<string.h>
 
 int a[10];
-char* cpuString(int eaxValues)
-{
-    if (eaxValues == 1) {
-        __asm__("mov $0x80000002, %eax");
-    }
-    else if (eaxValues == 2) {
-        __asm__("mov $0x80000003, %eax");
-    }
-    else if (eaxValues == 3) {
-        __asm__("mov $0x80000004, %eax");
-    }
-    __asm__("cpuid");
-    __asm__("mov %%eax, %0" : "=r" (a[0]));
-    __asm__("mov %%ebx, %0" : "=r" (a[1]));
-    __asm__("mov %%ecx, %0" : "=r" (a[2]));
-    __asm__("mov %%edx, %0" : "=r" (a[3]));
-    return (char*)&a[0];
-}
-
 char* getCpuID()
 {
     __asm__(
@@ -30,12 +11,25 @@ char* getCpuID()
         "xor %edx , %edx\n\t"
     );
 
-static char cpu_str[256] = "";
+    static char cpu_str[256] = "";
 
-for(int i=1; i<=3;i++){
-    char *strPtr =  cpuString(i);
-    strcat(cpu_str, strPtr);
-}
+    for (int i=1; i<=3; i++){
+        if (i == 1) {
+            __asm__("mov $0x80000002, %eax");
+        }
+        else if (i == 2) {
+            __asm__("mov $0x80000003, %eax");
+        }
+        else if (i == 3) {
+            __asm__("mov $0x80000004, %eax");
+        }
+        __asm__("cpuid");
+        __asm__("mov %%eax, %0" : "=r" (a[0]));
+        __asm__("mov %%ebx, %0" : "=r" (a[1]));
+        __asm__("mov %%ecx, %0" : "=r" (a[2]));
+        __asm__("mov %%edx, %0" : "=r" (a[3]));
+        strcat(cpu_str, (char*)&a[0]);
+    }
     
     return cpu_str;
 }

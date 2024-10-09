f1a3fc0c95568b47c53575c8c50f3980848ef09b
Kuttesch <moritzkuttesch@outlook.de>
2024-10-09 08:29:59 +0200

    updated systemArt.h

--- systemArt.h
+++ systemArt.h
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
 
-char* getSystemArt(char* systemArt){
+char* getSystemArt(OSVERSIONINFOEX osInfo, char* systemArt){
 #ifdef _WIN32
-    snprintf(systemArt, 256,"%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
-            "llllllllll  llllllllll",
-            "llllllllll  llllllllll",
-            "llllllllll  llllllllll",
-            "llllllllll  llllllllll",
+    DWORD osType;
+    GetProductInfo(osInfo.dwMajorVersion, osInfo.dwMinorVersion, osInfo.wServicePackMajor, osInfo.wServicePackMinor, &osType);
+    if (osInfo.dwMajorVersion == 10) {
+        snprintf(systemArt, 1024,"%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
+            "llllllllll llllllllll",
+            "llllllllll llllllllll",
+            "llllllllll llllllllll",
+            "llllllllll llllllllll",
             "",
-            "llllllllll  llllllllll",
-            "llllllllll  llllllllll",
-            "llllllllll  llllllllll",
-            "llllllllll  llllllllll");
+            "llllllllll llllllllll",
+            "llllllllll llllllllll",
+            "llllllllll llllllllll",
+            "llllllllll llllllllll");
+    }
     return systemArt;
 #endif
 }
